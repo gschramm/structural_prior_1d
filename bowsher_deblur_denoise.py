898d62faa96c59093dcc79382b804ce1f8de0cc7
Georg Schramm <gschra2@stanford.edu>
2022-10-13 11:44:49 -0700

    more realistic prior signal with noise

--- bowsher_deblur_denoise.py
+++ bowsher_deblur_denoise.py
@@ -11,37 +11,47 @@ from scipy.ndimage import gaussian_filter
 from operators import BowsherGradient1D, Convolution1D
 from functionals import SmoothNorm, TotalCost, L2NormSquared
 
+from scipy.ndimage import label, labeled_comprehension
+
 #----------------------------------------------------------------------------------------
 # input parameters
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--noise_level', default=0.15, type=float)
+parser.add_argument('--noise_level', default=0.06, type=float)
+parser.add_argument('--prior_noise_level', default=0.01, type=float)
 parser.add_argument('--n', default=256, type=int)
-parser.add_argument('--seed', default=0, type=int)
+parser.add_argument('--obj_seed', default=0, type=int)
+parser.add_argument('--noise_seed', default=0, type=int)
 parser.add_argument('--max_iter', default=500, type=int)
 parser.add_argument('--betas',
-                    default=[0, 0.01, 0.1, 1, 10, 100],
+                    default=[0.002, 0.2, 2, 20, 200],
                     type=float,
                     nargs='+')
 parser.add_argument('--neighbors',
                     default=[-3, -2, -1, 1, 2, 3],
                     type=int,
                     nargs='+')
-parser.add_argument('--num_nearest', default=2, type=int)
+parser.add_argument('--num_nearest', default=3, type=int)
 parser.add_argument('--sigma', default=1., type=float)
 parser.add_argument('--sigma_mismatched', default=0.7, type=float)
+parser.add_argument('--add_standalone', action='store_true')
+parser.add_argument('--add_extra_grad', action='store_true')
 args = parser.parse_args()
 print(args)
 
 noise_level: float = args.noise_level
+prior_noise_level: float = args.prior_noise_level
 n: int = args.n
-seed: int = args.seed
+obj_seed: int = args.obj_seed
+noise_seed: int = args.noise_seed
 num_iter: int = args.max_iter
 betas: npt.NDArray = np.array(args.betas)
 neighbors: tuple = tuple(args.neighbors)
 num_nearest: int = args.num_nearest
 sigma: float = args.sigma
 sigma_mismatched: float = args.sigma_mismatched
+add_standalone = args.add_standalone
+add_extra_grad = args.add_extra_grad
 
 data_norm: SmoothNorm = L2NormSquared()
 prior_norm: SmoothNorm = L2NormSquared()
@@ -52,7 +62,7 @@ kernel: npt.NDArray = np.exp(-(tmp**2) / (2 * sigma**2))
 kernel_mismatched: npt.NDArray = np.exp(-(tmp**2) / (2 * sigma_mismatched**2))
 
 #----------------------------------------------------------------------------------------
-np.random.seed(seed)
+np.random.seed(obj_seed)
 
 # normalize the kernel
 kernel /= kernel.sum()
@@ -70,16 +80,31 @@ x_true[-15:] = 0
 x_true /= x_true.max()
 
 # setup the prior signal
-prior_signal = (x_true.max() - x_true) * np.cos(3 * x_true)
-prior_signal[(n // 2):] *= -1
+prior_signal = (x_true.max() - x_true)  #* np.cos(3 * x_true)
+#prior_signal[(n // 2):] *= -1
 
-# add mismatch in prior signal
-prior_signal[180:185] = prior_signal.max()
-prior_signal[190:195] = prior_signal.max()
-prior_signal[200:205] = prior_signal.max()
+# find biggest flat regions to add extra edges in prior signal
+dx = x_true[1:] - x_true[:-1]
+label_arr, nlabs = label(dx == 0)
+labels = np.arange(1, nlabs)
+tmp = labeled_comprehension(x_true[:-1], label_arr, labels, lambda x: x.size,
+                            int, 0)
+max_label = labels[np.argmax(tmp)]
+
+if add_extra_grad:
+    start = np.where(label_arr == max_label)[0][0]
+    # add mismatch in prior signal
+    prior_signal[(start + 10):(start + 15)] += 0.2
+    prior_signal[(start + 20):(start + 25)] -= 0.2
 
 # add stand alone feature in true signal
-x_true[65:75] = 0.5
+if add_standalone:
+    tmp2 = tmp.copy()
+    tmp2[np.argmax(tmp)] = tmp.min()
+    max_label2 = labels[np.argmax(tmp2)]
+    start2 = np.where(label_arr == max_label2)[0]
+    start2 = start2[start2.shape[0] // 2]
+    x_true[(start2 - 5):(start2 + 5)] += 0.5
 
 # setup the data model
 data_operator = Convolution1D(n, kernel)
@@ -87,8 +112,11 @@ data_operator = Convolution1D(n, kernel)
 # generate noise-free data
 noise_free_data = data_operator.forward(x_true)
 
-noisy_data = noise_free_data + noise_level * x_true.mean() * np.random.randn(
+np.random.seed(noise_seed)
+noisy_data = noise_free_data + noise_level * x_true.max() * np.random.randn(
     *data_operator.x_shape)
+prior_signal += prior_noise_level * prior_signal.max() * np.random.randn(
+    *prior_signal.shape)
 
 #----------------------------------------------------------------------------------------
 # run the recons
@@ -203,7 +231,7 @@ for i, beta in enumerate(betas):
     ax[1, i].plot(np.arange(n),
                   recons_str_mismatched[i, :],
                   color='tab:red',
-                  label='it. recon')
+                  label='reconstruction')
 
     ax[0, i].set_title(f'prior weight {beta}', fontsize='medium')
 
@@ -274,12 +302,33 @@ ax3[0].fill_between(np.arange(n),
                     color=(0.8, 0.8, 0.8),
                     label='true signal')
 ax3[0].plot(prior_signal, 'k', label='structural signal', lw=0.5)
-ax3[0].legend(fontsize='small')
+ax3[0].legend(fontsize='small', loc='lower left')
 ax3[0].grid(ls=':')
+ax3[0].set_ylim(-0.3, None)
+
 ax3[1].plot(kernel, '.-', label='matched kernel')
 ax3[1].plot(kernel_mismatched, '.-', label='mismatched kernel')
 ax3[1].legend(fontsize='small')
 ax3[1].grid(ls=':')
+if add_extra_grad:
+    ax3[0].axvline(start + 10, ls='--', color=plt.cm.tab10(2), lw=0.5)
+    ax3[0].axvline(start + 25, ls='--', color=plt.cm.tab10(2), lw=0.5)
+    for axx in ax[1, :]:
+        axx.axvline(start + 10, ls='--', color=plt.cm.tab10(2), lw=0.5)
+        axx.axvline(start + 25, ls='--', color=plt.cm.tab10(2), lw=0.5)
+    for axx in ax2[1, :]:
+        axx.axvline(start + 10, ls='--', color=plt.cm.tab10(2), lw=0.5)
+        axx.axvline(start + 25, ls='--', color=plt.cm.tab10(2), lw=0.5)
+
+if add_standalone:
+    ax3[0].axvline(start2 - 5, ls='--', color=plt.cm.tab10(4), lw=0.5)
+    ax3[0].axvline(start2 + 5, ls='--', color=plt.cm.tab10(4), lw=0.5)
+    for axx in ax[1, :]:
+        axx.axvline(start2 - 5, ls='--', color=plt.cm.tab10(4), lw=0.5)
+        axx.axvline(start2 + 5, ls='--', color=plt.cm.tab10(4), lw=0.5)
+    for axx in ax2[1, :]:
+        axx.axvline(start2 - 5, ls='--', color=plt.cm.tab10(4), lw=0.5)
+        axx.axvline(start2 + 5, ls='--', color=plt.cm.tab10(4), lw=0.5)
 
 fig3.tight_layout()
 fig3.show()
